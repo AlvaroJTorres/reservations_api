c4b89cfa0b2678e5752f353171ae8ebec15deb42
AlvaroJTorres <mangetrone@gmail.com>
2022-08-29 09:33:29 -0500

    Fix bug on define_connection method

--- app/concepts/operations/user_operations/facebook_authenticate.rb
+++ app/concepts/operations/user_operations/facebook_authenticate.rb
@@ -8,13 +8,12 @@ module Operations
   module UserOperations
     # Operation for authenticate users log in or sing up from facebook
     class FacebookAuthenticate < Trailblazer::Operation
-      pass :define_connections
+      pass :define_connection
       pass :define_response
       pass :define_user_data
       pass :define_user
 
       def define_connection(options, **)
-        p 'DEFINE CONNECTION'
         options[:fb_conn] = Faraday.new(
           url: 'https://graph.facebook.com',
           headers: { 'Content-Type' => 'application/json' }
@@ -22,14 +21,12 @@ module Operations
       end
 
       def define_response(options, params:, **)
-        p 'DEFINE RESPONSE'
         options[:response] = options[:fb_conn].get('/me?fields=id,name,email') do |req|
           req.params['access_token'] = params[:assertion]
         end
       end
 
       def define_user_data(options, **)
-        p '***************************************** USER'
         raise StandardError, 'Empty Body' unless options[:response].body
 
         options[:user_data] = JSON.parse(options[:response].body)
@@ -39,7 +36,7 @@ module Operations
         options[:model] = if User.find_by(facebook_id: options[:user_data]['id'])
                             User.find_by(facebook_id: options[:user_data]['id'])
                           else
-                            result = Operations::UserOperations::FacebookCreate.call(params: options[:user_data])
+                            result = Operations::UserOperations::FacebookCreate.call(params: options[:user_data], connection: options[:fb_conn])
                             result[:model]
                           end
       end
