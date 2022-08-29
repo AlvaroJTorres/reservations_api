d7e3bf72af0c8c92692255d047c9894a1dc639b3
AlvaroJTorres <mangetrone@gmail.com>
2022-08-28 19:57:40 -0500

    Configurate application controller for doorkeeper requirements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
+# frozen_string_literal: true
+
+# Methods for all controllers
 class ApplicationController < ActionController::API
+  include ErrorHandler
+
+  def doorkeeper_unauthorized_render_options(*)
+    { json: { errors: ['Not authorized'] } }
+  end
+
+  def doorkeeper_forbidden_render_options(*)
+    { json: { errors: ['Not authorized'] } }
+  end
+
+  def current_user
+    @current_user = doorkeeper_authenticate || render_unauthorized
+  end
+
+  private
+
+  def render_unauthorized(message = 'Access denied')
+    errors = { errors: [message] }
+    render json: errors, status: :unauthorized
+  end
+
+  def doorkeeper_authenticate
+    User.find_by(id: doorkeeper_token[:resource_owner_id])
+  end
 end
