0041c6f68be8727d33868a3f9b16cc48c243fb99
AlvaroJTorres <mangetrone@gmail.com>
2022-08-28 19:55:45 -0500

    Configurate office hour model and associations

---
+++ app/models/office_hour.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class OfficeHour < ApplicationRecord
+  belongs_to :restaurant
+
+  enum day: { monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday: 7 }
+end
