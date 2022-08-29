9dd7ca5ff3d426f3d43aaeaafb02750fe0e9f9d2
AlvaroJTorres <mangetrone@gmail.com>
2022-08-28 19:56:34 -0500

    Add controller and actions for users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Controllers for User Model
+class UsersController < ApplicationController
+  before_action -> { doorkeeper_authorize! :admin }
+
+  def index
+    result = Operations::UserOperations::Index.call(params: {})
+    render json: { data: { users: result[:model] } }
+  end
+
+  def create
+    result = Operations::UserOperations::Create.call(params: user_params)
+    render json: { data: { user: result[:model] } }, status: :created
+  end
+
+  def update
+    result = Operations::UserOperations::Update.call(params: user_params, id: params[:id])
+    render json: { data: { user: result[:model] } }
+  end
+
+  private
+
+  def user_params
+    params.require(:data).permit(:name, :email, :role, :avatar, :restaurant_id)
+  end
+end
