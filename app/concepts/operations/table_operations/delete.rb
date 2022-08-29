21d6b1f8501f431490efb5b8a614455452039deb
AlvaroJTorres <mangetrone@gmail.com>
2022-08-28 20:03:39 -0500

    Add delete operation for table

---
+++ app/concepts/operations/table_operations/delete.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Operations
+  module TableOperations
+    # Operation to delete Tables on API
+    class Delete < Trailblazer::Operation
+      pass :find_restaurant
+      pass :find_table
+      step :delete_table
+
+      def find_restaurant(options, restaurant_id:, **)
+        options[:restaurant] = Restaurant.find_by!(id: restaurant_id)
+      end
+
+      def find_table(options, table_id:, **)
+        options[:model] = options[:restaurant].tables.find_by!(id: table_id)
+      end
+
+      def delete_table(options, **)
+        options[:model].deleted_at = Time.now
+        options[:model].save
+      end
+    end
+  end
+end
