016ec92d4976a9a6189f54136731d0dfc7ead408
AlvaroJTorres <mangetrone@gmail.com>
2022-08-28 19:57:54 -0500

    Update routes for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  use_doorkeeper
+
+  resources :restaurants do
+    resources :tables
+    resources :office_hours
+    resources :calendars
+  end
+  
+  resources :users
+  resources :reservations, only: %i[create]
+  get 'reservations/:customer_code', to: 'reservations#show'
+  patch 'reservations/:customer_code', to: 'reservations#update'
+  delete 'reservations/:customer_code', to: 'reservations#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
