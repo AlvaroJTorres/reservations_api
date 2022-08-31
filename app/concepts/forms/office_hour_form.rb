10ea935ac063e273fc4ddaaeb55efd2a929e6deb
AlvaroJTorres <mangetrone@gmail.com>
2022-08-30 20:28:58 -0500

    Add Validation for open close hours

--- app/concepts/forms/office_hour_form.rb
+++ app/concepts/forms/office_hour_form.rb
@@ -8,5 +8,23 @@ module Forms
     property :close
 
     validates :day, :open, :close, presence: true
+
+    validate :correct_open_hour
+
+    validate :correct_close_hour
+
+    private
+
+    def correct_open_hour
+      return if open < close
+
+      errors.add(:open, :open, message: 'Open hour must be before close hour')
+    end
+
+    def correct_close_hour
+      return if close > open
+
+      errors.add(:open, :open, message: 'Close hour must be after open hour')
+    end
   end
 end
