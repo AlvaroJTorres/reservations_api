e8641e6b85558c3844ee4b8eedc95362d081ca07
AlvaroJTorres <mangetrone@gmail.com>
2022-09-01 16:40:23 -0500

    Fix rubocop observations

--- app/controllers/customer/restaurants_controller.rb
+++ app/controllers/customer/restaurants_controller.rb
@@ -5,7 +5,8 @@ module Customer
   class RestaurantsController < Customer::ApplicationController
     def index
       result = Operations::RestaurantOperations::Index.call(params: {}, pagy_call: method(:pagy))
-      render json: { data: result[:records] }.merge!(meta: { pagination: pagy_headers_hash(result[:pagy]) }), status: :ok
+      render json: { data: result[:records] }.merge!(meta: { pagination: pagy_headers_hash(result[:pagy]) }),
+             status: :ok
     end
   end
 end
--- app/controllers/customer/tables_controller.rb
+++ app/controllers/customer/tables_controller.rb
@@ -4,8 +4,10 @@ module Customer
   # Controllers for Table Model
   class TablesController < Customer::ApplicationController
     def index
-      result = Operations::TableOperations::Index.call(params: query_params, restaurant_id: params[:restaurant_id], pagy_call: method(:pagy))
-      render json: { data: result[:records] }.merge!(meta: { pagination: pagy_headers_hash(result[:pagy]) }), status: :ok
+      result = Operations::TableOperations::Index.call(params: query_params, restaurant_id: params[:restaurant_id],
+                                                       pagy_call: method(:pagy))
+      render json: { data: result[:records] }.merge!(meta: { pagination: pagy_headers_hash(result[:pagy]) }),
+             status: :ok
     end
 
     private
