16a2a1cb859a2f5139ec792d779fc197b5bdd1c0
AlvaroJTorres <mangetrone@gmail.com>
2022-08-31 21:04:22 -0500

    Update routes with namespaces and rswag

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,31 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   use_doorkeeper
 
-  resources :restaurants do
-    resources :tables
-    resources :office_hours
-    resources :calendars
+  namespace :customer do
+    resources :restaurants, only: %i[index] do
+      resources :tables, only: %i[index]
+    end
+    resources :reservations, only: %i[create]
   end
   
-  resources :users
-  resources :reservations, only: %i[create]
-  get 'reservations/:customer_code', to: 'reservations#show'
-  patch 'reservations/:customer_code', to: 'reservations#update'
-  delete 'reservations/:customer_code', to: 'reservations#destroy'
+  namespace :admin do
+    resources :restaurants, only: %i[create update destroy]
+    resources :users, only: %i[index create update]
+  end
+
+  namespace :manager do
+    resources :restaurants do
+      resources :tables, only: %i[create destroy]
+      resources :office_hours, only: %i[create update destroy]
+      resources :calendars, only: %i[create update destroy]
+    end
+
+    get 'reservations/:customer_code', to: 'reservations#show'
+    patch 'reservations/:customer_code', to: 'reservations#update'
+    delete 'reservations/:customer_code', to: 'reservations#destroy'
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
