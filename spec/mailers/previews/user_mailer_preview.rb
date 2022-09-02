822ad125593e1be8644853e54abc2c145a7b4176
AlvaroJTorres <mangetrone@gmail.com>
2022-09-02 17:59:04 -0500

    Add fake data to the mailer previews

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -3,19 +3,35 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
   def customer_code_email
-    UserMailer.with(customer: User.find_by(role: 0), restaurant: Restaurant.find(1), reservation: Reservation.find(1)).customer_code_email
+    UserMailer.with(customer: customer, restaurant: restaurant, reservation: reservation).customer_code_email
   end
 
   def daily_report_email
-    UserMailer.with(manager: User.find_by(role: 2), reservations: 10, concurred_hours: concurred_hours).daily_report_email
+    UserMailer.with(manager: manager, reservations: 10, concurred_hours: concurred_hours).daily_report_email
   end
 
   private
 
+  def customer
+    Struct.new(:email, :name).new('jillsmith@example.com', 'Jill Smith')
+  end
+
+  def restaurant
+    OpenStruct.new(name: 'Test Restaurant', user: OpenStruct.new(email: 'manager@mail.com'))
+  end
+
+  def reservation
+    Struct.new(:customer_code, :datetime).new('1234567890', DateTime.new)
+  end
+
+  def manager
+    OpenStruct.new(name: 'Alberto Perez', email: 'manager@mail.com', restaurant: OpenStruct.new(name: 'Test Restaurant'))
+  end
+
   def concurred_hours
     {
-      '"05/09/2022-11:00"': 3,
-      '"05/09/2022-16:00"': 3
+      '11:00 AM': 3,
+      '04:00 PM': 3
     }
   end
 end
