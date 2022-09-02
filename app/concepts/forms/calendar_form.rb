a03c53b59da543ce09b019426d437ec4f6888b52
AlvaroJTorres <mangetrone@gmail.com>
2022-09-02 18:30:01 -0500

    Add validation for date format

--- app/concepts/forms/calendar_form.rb
+++ app/concepts/forms/calendar_form.rb
@@ -8,5 +8,13 @@ module Forms
     property :reason
 
     validates :restaurant_id, :date, :reason, presence: true
+
+    validate :date_format
+
+    private
+
+    def date_format
+      errors.add(:date, :date, message: 'Wrong date format') unless date =~ /\d{2}\/\d{2}\/\d{4}/
+    end
   end
 end
