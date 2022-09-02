4fa82bf96366f99ceb3c91ecae3e15e7e6dfd5dc
AlvaroJTorres <mangetrone@gmail.com>
2022-09-02 15:02:37 -0500

    Update facebook create operation to add image on user

--- app/concepts/operations/user_operations/facebook_create.rb
+++ app/concepts/operations/user_operations/facebook_create.rb
@@ -9,6 +9,9 @@ module Operations
       pass :new_model
       pass :user_data
       pass :save_model
+      pass :grab_image
+      pass :define_image_data
+      pass :attach_image_to_user
 
       def new_model(options, **)
         options[:model] = User.new
@@ -25,6 +28,22 @@ module Operations
 
       def save_model(options, **)
         options[:model].attributes = options[:user_data]
+        options[:model].save
+      end
+
+      def grab_image(options, connection:, assertion:, **)
+        options[:response] = connection.get("/#{options[:model].facebook_id}/picture?redirect=0") do |req|
+          req.params['access_token'] = assertion
+        end
+      end
+
+      def define_image_data(options, **)
+        options[:avatar] = JSON.parse(options[:response].body)['data']['url']
+        options[:avatar] = URI.parse(options[:avatar]).open
+      end
+
+      def attach_image_to_user(options, **)
+        options[:model].avatar.attach(io: options[:avatar], filename: "image#{options[:model].name}.jpg")
         options[:model].save
       end
     end
